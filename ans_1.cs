118effd1ad1cccc7d82e3e77c6050bf45ab04c35
CHINNAPATH <78593342+CHINNAPATH@users.noreply.github.com>
2021-06-13 14:10:14 +0700

    Add files via upload

---
+++ ans_1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, s, d, f, g, h, j, k, l, z;
+            Console.WriteLine();
+            a = int.Parse(Console.ReadLine());
+            s = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            f = int.Parse(Console.ReadLine());
+            g = int.Parse(Console.ReadLine());
+            h = int.Parse(Console.ReadLine());
+            j = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            l = int.Parse(Console.ReadLine());
+            z = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}", a, s, d, f, g, h, j, k, l,z);
+                    a = int.Parse(Console.ReadLine());
+                    s = int.Parse(Console.ReadLine());
+                    d = int.Parse(Console.ReadLine());
+                    f = int.Parse(Console.ReadLine());
+                    g = int.Parse(Console.ReadLine());
+                    h = int.Parse(Console.ReadLine());
+                    j = int.Parse(Console.ReadLine());
+                    k = int.Parse(Console.ReadLine());
+                    l = int.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+                Console.WriteLine("The number is available");
+                Console.Clear();
+            }
+            
+            
+            if (s == 0)
+            {
+                Console.WriteLine("The number is available");
+                
+            }
+            
+            
+            if (d == 0)
+            {
+                Console.WriteLine("The number is available");
+                
+            }
+            
+            
+            
+            if (d == 0)
+            {
+                Console.WriteLine("The number is available");
+                
+            }
+          
+            
+            
+            if (f == 0)
+            {
+                Console.WriteLine("The number is available");
+                
+
+            }
+          
+            
+            
+            if (g == 0)
+            {
+                Console.WriteLine("The number is available");
+                
+            }
+           
+            
+            
+            if (h == 0)
+            {
+                Console.WriteLine("The number is available");
+                
+            }
+           
+            
+            
+            if (j == 0)
+            {
+                Console.WriteLine("The number is available");
+                
+            }
+            
+            
+            
+            if (k == 0)
+            {
+                Console.WriteLine("The number is available");
+                
+            }
+           
+            
+            
+            if (l == 0)
+            {
+                Console.WriteLine("The number is available");
+                
+            }
+            else
+                Console.WriteLine("{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}",a, s, d, f, g, h, j, k, l);
+
+
+        }
+    }
+}
